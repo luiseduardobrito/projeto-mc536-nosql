c252a2b09e376d9ed35d4e6b058dbd39d650650f
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-11-10 18:25:42 -0200

    cache de artistas do autocomplete implementado em Redis

--- api/controllers/ArtistController.js
+++ api/controllers/ArtistController.js
@@ -1,22 +1,51 @@
 var request = require("request");
+var redis = require("redis");
+
+var redisClient = redis.createClient();
+// handle errors
+redisClient.on("error", function (err) {
+	console.log("Error " + err);
+});
 
 module.exports = {
 
 	autocomplete: function(req, res) {
 
 		var query = req.param("term");
-		var req_url = "http://developer.echonest.com/api/v4/artist/suggest?api_key=BB47HHMVRUJG6ZQSJ&results=5&name=" + query;
 
-		request(req_url, function(err, response, body) {
+		redisClient.hgetall(query, function(err, obj) {
 
-			var artists = JSON.parse(body).response.artists || [];
+			if(obj && obj.expires > Date.now()) {
 
-			var arr = [];
+				console.log("getting autocomplete from cache...");
 
-			for(var i = 0; i < artists.length; i++)
-				arr.push(artists[i].name);
+				var arr = JSON.parse(obj.values);
+				return res.json(arr);
+			}
 
-			res.json(arr);
-		});
+			else {
+
+				console.log("getting autocomplete from echonest...");
+
+				var req_url = "http://developer.echonest.com/api/v4/artist/suggest?api_key=BB47HHMVRUJG6ZQSJ&results=5&name=" + query;
+
+				request(req_url, function(err, response, body) {
+
+					var artists = JSON.parse(body).response.artists || [];
+
+					var arr = [];
+
+					for(var i = 0; i < artists.length; i++)
+						arr.push(artists[i].name);
+
+					// save in cache
+					redisClient.hset(query, "expires", Date.now() + 15*6000); // expires in 15 minutes
+					redisClient.hset(query, "values", JSON.stringify(arr)); // expires in 15 minutes
+
+					// serve response
+					res.json(arr);
+				});
+			}
+		})
 	}
 };
--- config/routes.js
+++ config/routes.js
@@ -32,10 +32,6 @@ module.exports.routes = {
   // located at `views/home/index.ejs`
   // 
   // (This would also work if you had a file at: `/views/home.ejs`)
-  '/': {
-    view: 'home/index'
-  },
-
   '/api/artist/autocomplete': 'Artist.autocomplete'
 
   /*
