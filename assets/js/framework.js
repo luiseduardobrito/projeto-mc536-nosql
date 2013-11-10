7e557bfaeebe39aa2e9121530289cc5db3236b0d
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-11-10 15:54:46 -0200

    pagina inicial atualizada

---
+++ assets/js/framework.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+	'ngRoute',
+	'myApp.filters',
+	'myApp.services',
+	'myApp.directives',
+	'myApp.controllers'
+])
+
+.config(['$routeProvider', function($routeProvider) {
+
+	$routeProvider
+		.when('/', {
+			templateUrl: '../../partials/home.html', 
+			controller: 'MyCtrl1'
+		});
+	
+	$routeProvider
+		.when('/login', {
+			templateUrl: '../../partials/login.html', 
+			controller: 'MyCtrl2'
+		});
+
+	$routeProvider
+		.otherwise({
+			redirectTo: '/view1'
+		});
+}]);
