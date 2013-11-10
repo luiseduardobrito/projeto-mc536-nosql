1a61df9f0010294b41cfcd01c5e42450682e67a4
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-11-10 18:51:11 -0200

    data mining criado baseado em Last.fm para artistas no Mongo

--- api/models/Artist.js
+++ api/models/Artist.js
@@ -1,19 +1,46 @@
-/**
- * Artist
- *
- * @module      :: Model
- * @description :: A short summary of how this model works and what it represents.
- * @docs		:: http://sailsjs.org/#!documentation/models
- */
+var request = require("request");
+
+var LASTFM_API_KEY = "8d081b73906ac02eb5471fb169844012";
+var LASTFM_SIMILAR_URL = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&api_key="+LASTFM_API_KEY+"&format=json&artist="
+var LASTFM_ARTIST_URL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key="+LASTFM_API_KEY+"&format=json&artist="
 
 module.exports = {
 
-  attributes: {
-  	
-  	/* e.g.
-  	nickname: 'string'
-  	*/
-    
-  }
+	attributes: {
 
+		name: "string",
+		wiki: "url",
+		bio: "string",
+		img: "url",
+		
+		tags: "array",
+		members: "array",
+		stats: "json",
+
+		getSimilar: function(cb) {
+
+			request(LASTFM_SIMILAR_URL + encodeURI(this.name), function(err, response, body) {
+				var artists = JSON.parse(body).similarartists.artist;
+				cb(artists);
+			})
+		}
+	},
+
+	beforeCreate: function(values, cb) {
+
+		request(LASTFM_ARTIST_URL + encodeURI(values.name), function(err, response, body) {
+
+			var artist = JSON.parse(body).artist;
+
+			// data mining
+			values.name = artist.name;
+			values.tags = artist.tags && artist.tags.tag ? artist.tags.tag : [];
+			values.bio = artist.bio && artist.bio.content ? artist.bio.content : null;
+
+			if(artist.image.length)
+				values.img = artist.image[artist.image.length - 1]["#text"];
+
+			cb();
+		})
+	}
 };
